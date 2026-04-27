cca6f84e77e3a7371ff71610d8227bf8766a7e71
Tkower <CarsonCaoTkower@gmail.com>
2026-04-27 18:35:01 +0200

    Add credit in opening scene
    add splash image
    change default cursor

--- Assets/Scripts/OpenningUI.cs
+++ Assets/Scripts/OpenningUI.cs
@@ -9,6 +9,8 @@ public class OpenningUI : MonoBehaviour
     public Button start;
     public Button quit;
     public Animator cameraMovement;
+    public Button credit;
+    private bool isCreditOpen;
 
     [Header("Scene")]
     public string sceneName = "GameScene";
@@ -19,16 +21,26 @@ public class OpenningUI : MonoBehaviour
 
     void Start()
     {
+        isCreditOpen = false;
         start.onClick.AddListener(StartGame);
         quit.onClick.AddListener(QuitGame);
+        credit.onClick.AddListener(Credit);
 
         SetFadeAlpha(0f);
+    }
+
+    void Credit()
+    {
+        isCreditOpen = !isCreditOpen;
+
+        cameraMovement.SetBool("Credit", isCreditOpen);
     }
 
     void StartGame()
     {
         start.interactable = false;
         quit.interactable = false;
+        credit.interactable = false;
 
         cameraMovement.SetBool("Started", true);
 
